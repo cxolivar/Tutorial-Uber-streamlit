ac9758bcb1960aaef510b231c8109e717be70385
cxolivar <cxolivar@gmail.com>
2024-08-30 09:35:25 -0400

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -34,7 +34,7 @@ if st.checkbox('Show raw data'):
     st.subheader('Raw data')
     st.write(data)
 
-st.subheader('Number of pickups by hour')
+st.subheader('Number of pickups by hour pichulgjhgjgita')
 hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
 st.bar_chart(hist_values)
